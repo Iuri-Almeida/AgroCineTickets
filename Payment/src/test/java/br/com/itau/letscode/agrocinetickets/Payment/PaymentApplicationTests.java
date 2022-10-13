3a6a71a504ef2ad05c1133885f5c89ad2abe162f
Iuri Almeida <iurilopesalmeida@gmail.com>
2022-10-13 01:06:25 -0300

    feat(payment): add Payment project

---
+++ Payment/src/test/java/br/com/itau/letscode/agrocinetickets/Payment/PaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.itau.letscode.agrocinetickets.Payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
