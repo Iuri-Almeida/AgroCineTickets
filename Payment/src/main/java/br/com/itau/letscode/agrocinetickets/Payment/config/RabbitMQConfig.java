174af490f87d177816895f06f8bfc045d2c055ff
Iuri Almeida <iurilopesalmeida@gmail.com>
2022-10-13 01:39:12 -0300

    feat(payment): add RabbitMQ configurations

---
+++ Payment/src/main/java/br/com/itau/letscode/agrocinetickets/Payment/config/RabbitMQConfig.java
@@ -0,0 +1,111 @@
+package br.com.itau.letscode.agrocinetickets.Payment.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String PAYMENTS_TICKETS_QUEUE_AND_DIRECT_EXCHANGE = "payments.tickets";
+    public static final String TICKETS_PAYMENTS_QUEUE = "tickets.payments";
+    public static final String PAYMENTS_ROOMS_QUEUE = "payments.rooms";
+    public static final String PAYMENTS_TICKETS_ROOMS_FANOUT_EXCHANGE = "payments.tickets_rooms";
+
+    @Value("${spring.rabbitmq.host}")
+    String host;
+
+    @Value("${spring.rabbitmq.port}")
+    int port;
+
+    @Value("${spring.rabbitmq.username}")
+    String username;
+
+    @Value("${spring.rabbitmq.password}")
+    String password;
+
+    @Bean
+    public Queue ticketsPaymentsQueue() {
+        return new Queue(TICKETS_PAYMENTS_QUEUE);
+    }
+
+    @Bean
+    public DirectExchange paymentsTicketsExchange() {
+        return new DirectExchange(PAYMENTS_TICKETS_QUEUE_AND_DIRECT_EXCHANGE);
+    }
+
+    @Bean
+    public FanoutExchange paymentsTicketsAndRoomsExchange() {
+        return new FanoutExchange(PAYMENTS_TICKETS_ROOMS_FANOUT_EXCHANGE);
+    }
+
+    @Bean
+    public Queue paymentsTicketsQueue() {
+        return new Queue(PAYMENTS_TICKETS_QUEUE_AND_DIRECT_EXCHANGE);
+    }
+
+    @Bean
+    public Queue paymentsRoomsQueue() {
+        return new Queue(PAYMENTS_ROOMS_QUEUE);
+    }
+
+    @Bean
+    public Binding paymentsTicketsBinding(
+            Queue paymentsTicketsQueue,
+            DirectExchange paymentsTicketsExchange
+    ) {
+        return BindingBuilder
+                .bind(paymentsTicketsQueue)
+                .to(paymentsTicketsExchange)
+                .with("");
+    }
+
+    @Bean
+    public Binding paymentsTicketsFanoutBinding(
+            Queue paymentsTicketsQueue,
+            FanoutExchange paymentsTicketsAndRoomsExchange
+    ) {
+        return BindingBuilder
+                .bind(paymentsTicketsQueue)
+                .to(paymentsTicketsAndRoomsExchange);
+    }
+
+    @Bean
+    public Binding paymentsRoomsFanoutBinding(
+            Queue paymentsRoomsQueue,
+            FanoutExchange paymentsTicketsAndRoomsExchange
+    ) {
+        return BindingBuilder
+                .bind(paymentsRoomsQueue)
+                .to(paymentsTicketsAndRoomsExchange);
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory();
+        cachingConnectionFactory.setHost(host);
+        cachingConnectionFactory.setPort(port);
+        cachingConnectionFactory.setUsername(username);
+        cachingConnectionFactory.setPassword(password);
+        return cachingConnectionFactory;
+    }
+
+    @Bean
+    public RabbitTemplate template(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(this.messageConverter());
+        return template;
+    }
+
+}
---
+++ Payment/src/main/java/br/com/itau/letscode/agrocinetickets/Payment/listener/TicketsPaymentsListener.java
@@ -0,0 +1,35 @@
+package br.com.itau.letscode.agrocinetickets.Payment.listener;
+
+import br.com.itau.letscode.agrocinetickets.Payment.config.RabbitMQConfig;
+import br.com.itau.letscode.agrocinetickets.Payment.model.Ticket;
+import br.com.itau.letscode.agrocinetickets.Payment.model.enums.TicketStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+
+@Component
+@RequiredArgsConstructor
+public class TicketsPaymentsListener {
+
+    private final RabbitTemplate template;
+
+    @RabbitListener(queues = RabbitMQConfig.TICKETS_PAYMENTS_QUEUE)
+    public void listener(Ticket ticket) {
+        boolean successfulPayment = new Random().nextInt(100) < 80;
+        String exchange;
+
+        if (successfulPayment) {
+            ticket.setStatus(TicketStatus.SUCCESS);
+            exchange = RabbitMQConfig.PAYMENTS_TICKETS_QUEUE_AND_DIRECT_EXCHANGE;
+        } else {
+            ticket.setStatus(TicketStatus.ERROR);
+            exchange = RabbitMQConfig.PAYMENTS_TICKETS_ROOMS_FANOUT_EXCHANGE;
+        }
+
+        template.convertAndSend(exchange, "", ticket);
+    }
+
+}
