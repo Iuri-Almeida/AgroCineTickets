bbd156792a067f8a316521d20b8753079ede11cb
Iuri Almeida <iurilopesalmeida@gmail.com>
2022-10-13 00:25:07 -0300

    feat(session): remove unnecessary methods

--- Session/src/main/java/br/com/itau/letscode/agrocinetickets/Session/controller/SessionController.java
+++ Session/src/main/java/br/com/itau/letscode/agrocinetickets/Session/controller/SessionController.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -16,24 +15,9 @@ public class SessionController {
 
     private final SessionService sessionService;
 
-    @GetMapping
-    public ResponseEntity<List<Session>> findAll() {
-        return ResponseEntity.ok().body(sessionService.findAll());
-    }
-
     @GetMapping(value = "/{id}")
     public ResponseEntity<Session> findById(@PathVariable UUID id) {
         return ResponseEntity.ok().body(sessionService.findById(id));
-    }
-
-    @PostMapping(value = "/commit")
-    public ResponseEntity<Void> chooseSeat(
-            @RequestBody Session session,
-            @RequestParam(value = "line", defaultValue = "") int line,
-            @RequestParam(value = "column", defaultValue = "") int column
-    ) {
-        sessionService.chooseSeat(session, line, column);
-        return ResponseEntity.noContent().build();
     }
 
 }
