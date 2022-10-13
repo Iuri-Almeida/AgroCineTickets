78de9afd92c3c34ed02eaf7f0ad46c56bfde8b99
Iuri Almeida <iurilopesalmeida@gmail.com>
2022-10-13 16:00:57 -0300

    fix(ticket): change HTTP method

--- Ticket/src/main/java/br/com/itau/letscode/agrocinetickets/Ticket/client/RoomsClient.java
+++ Ticket/src/main/java/br/com/itau/letscode/agrocinetickets/Ticket/client/RoomsClient.java
@@ -2,10 +2,7 @@ package br.com.itau.letscode.agrocinetickets.Ticket.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -19,7 +16,7 @@ public interface RoomsClient {
             @RequestParam(value = "column", defaultValue = "") int column
     );
 
-    @PatchMapping(value = "/rooms/{id}/occupySeat")
+    @PutMapping(value = "/rooms/{id}/occupySeat")
     ResponseEntity<Void> occupySeat(
             @PathVariable UUID id,
             @RequestParam(value = "line", defaultValue = "") int line,
