bc97f133e0130aa18d3a9aaacac0206027cc5033
Iuri Almeida <iurilopesalmeida@gmail.com>
2022-10-13 02:23:24 -0300

    feat(room): add RabbitMQ configurations

---
+++ Room/src/main/java/br/com/itau/letscode/agrocinetickets/Room/exception/SessionNullPointerException.java
@@ -0,0 +1,7 @@
+package br.com.itau.letscode.agrocinetickets.Room.exception;
+
+public class SessionNullPointerException extends RuntimeException {
+    public SessionNullPointerException(String msg) {
+        super(msg);
+    }
+}
