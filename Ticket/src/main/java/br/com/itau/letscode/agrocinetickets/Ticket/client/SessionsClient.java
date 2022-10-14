519d9fdb74a86b90aa15614ef5bbae1c6709028e
Iuri Almeida <iurilopesalmeida@gmail.com>
2022-10-14 19:18:57 -0300

    refactor(ticket): remove Room endpoints

--- Ticket/src/main/java/br/com/itau/letscode/agrocinetickets/Ticket/client/SessionsClient.java
+++ Ticket/src/main/java/br/com/itau/letscode/agrocinetickets/Ticket/client/SessionsClient.java
@@ -5,6 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.UUID;
 
@@ -13,5 +15,19 @@ public interface SessionsClient {
 
     @GetMapping(value = "/sessions/{id}")
     ResponseEntity<Session> findById(@PathVariable UUID id);
+
+    @GetMapping(value = "/sessions/{id}/isSeatOccupied")
+    ResponseEntity<Boolean> isSeatOccupied(
+            @PathVariable UUID id,
+            @RequestParam(value = "line", defaultValue = "") int line,
+            @RequestParam(value = "column", defaultValue = "") int column
+    );
+
+    @PutMapping(value = "/sessions/{id}/occupySeat")
+    ResponseEntity<Void> occupySeat(
+            @PathVariable UUID id,
+            @RequestParam(value = "line", defaultValue = "") int line,
+            @RequestParam(value = "column", defaultValue = "") int column
+    );
 
 }
